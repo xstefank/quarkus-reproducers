4a5739f209579025e2773561129dbdbef7ed5a48
xstefank <xstefank122@gmail.com>
2023-12-21 14:01:57 +0100

    Fix hc-messages for new repoducer

--- hc-messages/src/main/java/org/acme/ExtendedDBHC.java
+++ hc-messages/src/main/java/org/acme/ExtendedDBHC.java
@@ -1,19 +1,14 @@
 package org.acme;
 
 import io.quarkus.agroal.runtime.health.DataSourceHealthCheck;
-import io.smallrye.health.api.HealthGroup;
 import org.eclipse.microprofile.health.HealthCheckResponse;
-import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.health.Liveness;
 
-@Readiness
+@Liveness
 public class ExtendedDBHC extends DataSourceHealthCheck {
 
     @Override
     public HealthCheckResponse call() {
-        HealthCheckResponse result = super.call();
-//        if (result.getStatus().equals(HealthCheckResponse.Status.DOWN)) {
-            System.out.println("Send message here");
-//        }
-        return result;
+        return super.call();
     }
 }
