dc274a2447b9ff2406d2153e60cff1c6d5d6cf84
xstefank <xstefank122@gmail.com>
2024-06-20 07:30:31 +0200

    Add messaging-reload

---
+++ messaging-reload/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,32 @@
+package org.acme;
+
+import io.smallrye.mutiny.Multi;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import java.time.Duration;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+
+    @Outgoing("topic")
+    public Multi<String> produce() {
+        return Multi.createFrom().ticks().every(Duration.ofMillis(100))
+            .map(t -> "Event at " + t);
+    }
+
+    @Incoming("topic")
+    public void consume(String message) {
+        System.out.println(message + " received");
+    }
+}
