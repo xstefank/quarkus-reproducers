1b2caf3ad2b4c194248abdcc950989a0ec4b1efa
xstefank <xstefank122@gmail.com>
2024-07-08 10:14:13 +0200

    Add mdc-propagation

---
+++ mdc-propagation/src/main/java/org/acme/TestHC2.java
@@ -0,0 +1,18 @@
+package org.acme;
+
+import io.quarkus.logging.Log;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+import org.jboss.logging.MDC;
+
+@Liveness
+public class TestHC2 implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        MDC.put("health-check", getClass().getSimpleName());
+        Log.info("Health check");
+        return HealthCheckResponse.up("test-hc2");
+    }
+}
