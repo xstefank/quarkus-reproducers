02ae5b02384bcd36d593d10000d16fcb1eb8139e
xstefank <xstefank122@gmail.com>
2024-09-16 10:18:20 +0200

    Add woprking single request/response case to bidi streaming reproducer

--- grpc-bidi-streaming/src/main/java/org/acme/GrpcTestService.java
+++ grpc-bidi-streaming/src/main/java/org/acme/GrpcTestService.java
@@ -3,10 +3,16 @@ package org.acme;
 
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 
 @GrpcService
 public class GrpcTestService implements Streaming {
 
+
+    @Override
+    public Uni<Item> pipe1(Item request) {
+        return Uni.createFrom().item(() -> Item.newBuilder().setValue(request.getValue().toUpperCase()).build());
+    }
 
     @Override
     public Multi<Item> pipe(Multi<Item> request) {
