f3be5ba88c91a6b0e429f14e5437a95f8268ebdf
xstefank <xstefank122@gmail.com>
2024-09-17 16:56:36 +0200

    Add more test methods to bidi-streaming

--- grpc-bidi-streaming/src/main/java/org/acme/GrpcTestService.java
+++ grpc-bidi-streaming/src/main/java/org/acme/GrpcTestService.java
@@ -2,21 +2,40 @@ package org.acme;
 
 
 import io.quarkus.grpc.GrpcService;
+import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
+
+import java.util.List;
 
 @GrpcService
 public class GrpcTestService implements Streaming {
 
 
     @Override
-    public Uni<Item> pipe1(Item request) {
+    public Multi<Item> bidiStreaming(Multi<Item> request) {
+        return request.map(item ->
+            Item.newBuilder().setValue(item.getValue().toUpperCase()).build());
+    }
+
+    @Override
+    public Uni<Item> unary(Item request) {
         return Uni.createFrom().item(() -> Item.newBuilder().setValue(request.getValue().toUpperCase()).build());
     }
 
     @Override
-    public Multi<Item> pipe(Multi<Item> request) {
-        return request.map(item ->
-            Item.newBuilder().setValue(item.getValue().toUpperCase()).build());
+    public Multi<Item> serverStreaming(Item request) {
+        Log.error("serverStreaming");
+        return Multi.createFrom().items(
+            Item.newBuilder().setValue(request.getValue().toUpperCase()).build(),
+            Item.newBuilder().setValue(request.getValue().toUpperCase()).build(),
+            Item.newBuilder().setValue(request.getValue().toUpperCase()).build()
+        );
+    }
+
+    @Override
+    public Uni<Item> clientStreaming(Multi<Item> request) {
+        Log.error("clientStreaming");
+        return request.map(item -> item.getValue().toUpperCase()).collect().asList().onItem().transformToUni(l -> Uni.createFrom().item(() -> Item.newBuilder().setValue(String.join(" - ", l)).build()));
     }
 }
