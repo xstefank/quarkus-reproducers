4c15a89ba165b4766323d9ab271eff31a53ed214
xstefank <xstefank122@gmail.com>
2024-12-09 09:33:51 +0100

    Add QuarkusApplication to multiple-mains

--- multiple-mains/src/main/java/org/acme/Main1.java
+++ multiple-mains/src/main/java/org/acme/Main1.java
@@ -1,13 +1,23 @@
 package org.acme;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 //@QuarkusMain
 public class Main1 {
 
     public static void main(String[] args) {
-        System.out.println("Main1");
-        Quarkus.run(args);
+        Quarkus.run(MyMain1.class, args);
+    }
+
+    public static class MyMain1 implements QuarkusApplication {
+
+        @Override
+        public int run(String... args) throws Exception {
+            System.out.println("Indeed in Main1");
+            Quarkus.waitForExit();
+            return 0;
+        }
     }
 }
--- multiple-mains/src/main/java/org/acme/Main2.java
+++ multiple-mains/src/main/java/org/acme/Main2.java
@@ -1,14 +1,24 @@
 package org.acme;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 //@QuarkusMain
 public class Main2 {
 
     public static void main(String[] args) {
-        System.out.println("Main2");
         System.setProperty("quarkus.http.port", "8081");
-        Quarkus.run(args);
+        Quarkus.run(MyMain2.class, args);
+    }
+
+    public static class MyMain2 implements QuarkusApplication {
+
+        @Override
+        public int run(String... args) throws Exception {
+            System.out.println("Indeed in Main2");
+            Quarkus.waitForExit();
+            return 0;
+        }
     }
 }
