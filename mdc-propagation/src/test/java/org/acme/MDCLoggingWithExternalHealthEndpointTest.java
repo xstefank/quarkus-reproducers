a5cd454219baa73f5c0583f7e2657678d5b8b939
Atanas Nikov <atanas.nikov@ft.com>
2025-05-05 18:18:50 +0300

    Reproduce MDC context propagation issue with an external health endpoint

---
+++ mdc-propagation/src/test/java/org/acme/MDCLoggingWithExternalHealthEndpointTest.java
@@ -0,0 +1,30 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.RepeatedTest;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+public class MDCLoggingWithExternalHealthEndpointTest {
+
+  @Inject
+  InMemoryLogHandler inMemoryLogHandler;
+  @Inject
+  InMemoryLogHandlerProducer producer;
+
+  @RepeatedTest(10)
+  void testMDCLoggingWithExternalHealthEndpoint() {
+    InMemoryLogHandler.reset();
+
+    given().get("/health").then().statusCode(200);
+
+    assertThat(inMemoryLogHandler.logRecords())
+        .hasSize(4)
+        .anyMatch(record -> record.contains("health-check=TestHC"))
+        .anyMatch(record -> record.contains("health-check=TestHC2"))
+        .anyMatch(record -> record.contains("health-check=AsyncTestHC"))
+        .anyMatch(record -> record.contains("health-check=AsyncTestHC2"));
+  }
+}
