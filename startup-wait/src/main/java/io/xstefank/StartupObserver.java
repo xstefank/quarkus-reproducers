b7469859648b45378d9570c97bc9912b19f41769
xstefank <xstefank122@gmail.com>
2025-12-03 10:42:19 +0100

    Add startup-wait
    Signed-off-by: xstefank <xstefank122@gmail.com>

---
+++ startup-wait/src/main/java/io/xstefank/StartupObserver.java
@@ -0,0 +1,34 @@
+package io.xstefank;
+
+import io.quarkus.runtime.Startup;
+import io.quarkus.runtime.StartupEvent;
+import io.vertx.core.Vertx;
+import io.vertx.core.WorkerExecutor;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Singleton;
+
+@Singleton
+@Startup
+public class StartupObserver {
+
+    private final WorkerExecutor workerExecutor;
+
+    StartupObserver(Vertx vertx) {
+        workerExecutor = vertx.createSharedWorkerExecutor("startup-executor");
+    }
+
+    public void onStartup(@Observes StartupEvent event) {
+        System.out.println("StartupObserver.onStartup");
+        workerExecutor.executeBlocking(promise -> {
+            System.out.println("in promise");
+            try {
+                Thread.sleep(60000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("promise complete");
+            promise.complete();
+        });
+        System.out.println("StartupObserver.onStartup finished");
+    }
+}
